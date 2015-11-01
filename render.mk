411e8a73bb3e39f7889a91f7e94f8ece914d63aa
Snuzzo <jrracinfan@gmail.com>
2015-11-01 11:56:24 -0500

    Move ZIMAGE definition

--- render.mk
+++ render.mk
@@ -5,7 +5,6 @@ PRODUCT_DEFCONFIG := clark_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/motorola/msm8992
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 ARCH := arm64
-ZIMAGE := arch/arm64/boot/Image
 ZIP_FILES_DIR := device/motorola/clark/anykernel_installer
 
 TARGET_REQUIRES_DTB := true
